28504e0017e57b0765a060d56cfb02f3ba2c8f7b
farouk0197 <fkatakpaou@gmail.com>
2022-09-01 15:02:34 -0400

    feat: updated background images, implementating the difficulties

--- script.js
+++ script.js
@@ -1,36 +1,76 @@
 kaboom()
 
 //Assets
-loadSprite("birdy", "images/birdy.png");
+loadSprite("mj", "images/mj.png");
+loadSprite("bulls", "images/Bulls.png");
 loadSprite("bg", "images/bg.png");
 loadSprite("pipe", "images/pipe.png");
+loadSprite("giphy", "images/giphy.gif");
 loadSound("download", "audio/download.mp3");
 loadSound("weak", "audio/weak.mp3");
 loadSound("score", "audio/score.mp3");
-
+loadSound("halo", "audio/halo.mp3");
 let highScore = 0;
-let hop = 400
+let hop = 400;
+let gap = 180;
+
+scene("easy", () => {
+  hop = 400;
+  gap = 180;
+  go("game")
+})
+
+
+scene("medium", () => {
+  hop = 350;
+  gap = 120;
+  go("game")
+})
+
+scene("hard", () => {
+  hop = 280;
+  gap = 100;
+  go("game")
+})
+
 
 scene("intro", () => {
   add([
+    sprite("bg", {width: width(), height: height()})
+  ]);
+  add([
     text(
-      "Start (Press Space Bar)!\n",
+      "Start (Press Space Bar for easy)\n"
+      + "(press enter for meduim)\n"+
+      "(press tab for hard)",
       {size: 45}
     )
+    
+
   ]);
 
   keyPress("space", () => {
-    go("game");
+    go("easy");
   });
+
+  keyPress("enter", () => {
+    go("medium");
+  });
+
+  keyPress("tab", () => {
+    go("hard");
+  });
+
+  
 });
 
 scene("game", () => {
-  const PIPE_GAP = 140;
+  let PIPE_GAP = gap;
   let score = 0;
 
 
   add([
-    sprite("bg", {width: width(), height: height()})
+    sprite("bulls", {width: width(), height: height()})
   ]);
 
   const scoreText = add([
@@ -40,15 +80,15 @@ scene("game", () => {
   // add a game object to screen
   const player = add([
     // list of components
-    sprite("birdy"),
-    scale(3),
+    sprite("mj"),
+    scale(.07),
     pos(80, 40),
     area(),
     body(),
   ]);
 
   function producePipes(){
-    const offset = rand(-200, 200);
+    const offset = rand(-80, 80);
 
     add([
       sprite("pipe"),
@@ -81,6 +121,7 @@ scene("game", () => {
       pipe.passed = true;
       score += 1;
       scoreText.text = score;
+      play("score")
     }
   });
 
@@ -90,7 +131,8 @@ scene("game", () => {
   });
 
   player.action(() => {
-    if (player.pos.y > height() + 30 || player.pos.y < -30) {
+    if (player.pos.y > height() + 120 || player.pos.y < -120) {
+      play("halo");
       go("gameover", score);
     }
   });
@@ -99,18 +141,22 @@ scene("game", () => {
     play("download"); 
     player.jump(hop);
   });
+  
 });
 
 scene("gameover", (score) => {
   if (score > highScore) {
     highScore = score;
   }
-
+  add([
+    sprite("giphy", {width: width(), height: height()})
+  ]);
   add([
     text(
       "gameover!\n"
       + "score: " + score
-      + "\nhigh score: " + highScore,
+      + "\nhigh score: " + highScore + "\n(press space to try again)" + 
+      "\n(press enter to main menu)",
       {size: 45}
     )
   ]);
@@ -118,6 +164,12 @@ scene("gameover", (score) => {
   keyPress("space", () => {
     go("game");
   });
+
+  keyPress("enter", () => {
+    highScore = 0;
+    go("intro");
+  });
+
 });
 
 go("intro");
